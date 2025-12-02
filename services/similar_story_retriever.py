d9f26307649b2f67e9d1c0bf26d0cbb0657ec027
Ricky Wong <rickywck@gmail.com>
2025-12-02 14:21:04 -0500

    Remove unused package

--- services/similar_story_retriever.py
+++
@@ -1,7 +0,0 @@
-"""Deprecated module; use `agents.utils.similar_story_retriever` instead."""
-import logging
-from agents.utils.similar_story_retriever import SimilarStoryRetriever  # noqa: F401
-
-logging.getLogger(__name__).warning(
-    "services.similar_story_retriever is deprecated; import from agents.utils.similar_story_retriever"
-)
